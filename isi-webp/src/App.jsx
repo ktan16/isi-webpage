52b71ec70b39c395408b0dae937e12a022a91468
kennymbp <kennysjtan@gmail.com>
2024-10-28 15:36:49 -0700

    title implemented, change colors, init about

--- isi-webp/src/App.jsx
+++ isi-webp/src/App.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Top from "./components/Top/Top";
+import About from "./components/About/About";
+import Title from "./components/Title/Title";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Top />
+      <div className="container">
+        <Title subTitle="About Us" title="Who We Are" />
+        <About />
+      </div>
     </div>
   );
 }
