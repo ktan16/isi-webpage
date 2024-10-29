a26a81133a056923e2f679aebe7fd72095bbce4e
kennymbp <kennysjtan@gmail.com>
2024-10-29 01:22:16 -0700

    implement about and dark nav

--- isi-webp/src/components/About/About.jsx
+++ isi-webp/src/components/About/About.jsx
@@ -1,10 +1,60 @@
 import React from "react";
 import "./About.css";
+import about1 from "../../assets/about1.jpg";
+import about2 from "../../assets/about2.jpg";
 
 const About = () => {
   return (
-    <div>
-      <h1>hello</h1>
+    <div className="about">
+      <div className="about-what">
+        <div className="about-what-left">
+          <img src={about1} alt="" className="about-what-img" />
+        </div>
+
+        <div className="about-what-right">
+          <h2>What We Do</h2>
+          <h1>Strength that lasts</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum
+            accusantium natus sequi. Nostrum ratione error eum enim perferendis
+            hic porro, facere blanditiis nihil repellendus necessitatibus optio
+            esse saepe quo fugiat quisquam laborum obcaecati cupiditate non et
+            neque. Consectetur ad excepturi incidunt distinctio molestias
+            reprehenderit facere, iste error illo ab, quae ex aperiam illum
+            assumenda, deserunt explicabo praesentium quo vero expedita
+            provident? Omnis maiores, eaque dolor esse fuga reprehenderit, culpa
+            quidem veritatis necessitatibus repellendus praesentium sapiente
+            obcaecati id earum odit officiis nam? Nemo incidunt at deserunt vel
+            molestiae! Suscipit sequi temporibus atque accusamus deleniti,
+            minima alias assumenda excepturi totam quasi illo.
+          </p>
+        </div>
+      </div>
+
+      <div className="about-why">
+        <div className="about-why-left">
+          <h2>Why We Do It</h2>
+          <h1>Support you can trust</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus
+            tempora molestiae alias ipsum quibusdam expedita totam id eum
+            facere, aliquid sunt autem provident quos, incidunt repudiandae
+            suscipit corporis excepturi nam, inventore odit temporibus eaque.
+            Perferendis delectus aliquid error ipsa unde, natus officiis
+            expedita saepe, nostrum harum aperiam provident voluptates sed odio
+            illo optio animi dolorem quia quam maxime voluptate eaque velit
+            iusto. Non cupiditate qui minima consectetur sapiente impedit fugiat
+            laborum dolores velit ipsam laudantium atque adipisci accusamus in
+            earum nisi reiciendis esse, reprehenderit facere numquam ducimus!
+            Alias, sapiente maxime? Deleniti aliquid blanditiis magni sed dolore
+            voluptatem repudiandae obcaecati sapiente.
+          </p>
+        </div>
+
+        <div className="about-why-right">
+          <img src={about2} alt="" className="about-what-img" />
+        </div>
+      </div>
     </div>
   );
 };
--- isi-webp/src/components/Navbar/Navbar.jsx
+++ isi-webp/src/components/Navbar/Navbar.jsx
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Navbar.css";
 import isi_logo from "../../assets/isi_short_logo.png";
+import isi_logo_dark from "../../assets/isi_short_logo_dark.png";
 
 const Navbar = () => {
+  const [sticky, setSticky] = useState(false); // sticky state for changing nav bar color
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      // event listener for scrolling
+      window.scrollY > 810 ? setSticky(true) : setSticky(false); // if scroll more than 800px, dark navbar
+    });
+  }, []);
+
   return (
-    <nav className="navbar container">
-      <img src={isi_logo} alt="" className="logo" />
+    <nav className={`navbar container ${sticky ? "dark-nav" : ""}`}>
+      <img
+        src={`${sticky ? isi_logo_dark : isi_logo}`}
+        alt=""
+        className="logo"
+      />
       <h1>International Synthetic</h1>
       <h2>INDUSTRIES, INC.</h2>
       <ul>
-        <li>Main</li>
+        <li>Home</li>
 
         <li>About</li>
 
