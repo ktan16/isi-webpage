82e420457f60789d5101284c7961e93bead70677
kennymbp <kennysjtan@gmail.com>
2025-03-11 23:30:44 -0700

    init contact

--- isi-webp/src/components/Contact/Contact.jsx
+++ isi-webp/src/components/Contact/Contact.jsx
@@ -1,9 +1,42 @@
 import React from "react";
+import "./Contact.css";
 
 const Contact = () => {
   return (
-    <div>
-      <div>hello</div>
+    <div className="contact">
+      <div className="contact-col">
+        <h3>Send us a message</h3>
+        <p>
+          Inquire using our contact form, or use our contact information below.
+          Your feedback, questions, and suggestions are essential to our ability
+          to provide exceptional service.
+        </p>
+
+        <ul>
+          <li>
+            <span className="label">Email:</span>
+            <br />
+            <span className="value">isi.pcc_localsale@yahoo.com</span>
+          </li>
+
+          <li>
+            <span className="label">Phone Number:</span>
+            <br />
+            <span className="value">+63 917 629 7708</span>
+          </li>
+
+          <li>
+            <span className="label">Address</span>
+            <br />
+            <span className="value">
+              389 Elpidio Quirino Highway, Barangay Talipapa, Novaliches, Quezon
+              City, Philippines
+            </span>
+          </li>
+        </ul>
+      </div>
+
+      <div className="contact-col"></div>
     </div>
   );
 };
