be0dd1040210076ccf345694d8d7b14b254d1615
Kenny Tan <kennysjtan@gmail.com>
2025-05-04 12:11:36 -0700

    finish comments

--- isi-webp/src/App.jsx
+++ isi-webp/src/App.jsx
@@ -1,3 +1,6 @@
+// App.jsx
+// Main application. Uses react-scroll to navigate.
+
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
@@ -5,25 +8,34 @@ import About from "./components/About/About";
 import Title from "./components/Title/Title";
 import Gallery from "./components/Gallery/Gallery";
 import Contact from "./components/Contact/Contact";
-import { Element } from "react-scroll";
+import { Element } from "react-scroll"; // Enables smooth scrolling
 
 function App() {
   return (
     <div>
+      {/* Sticky nav bar */}
       <Navbar />
+
+      {/* Hero section with main image of rope and quote. */}
+      {/* Scroll anchor: home */}
       <Element name="home">
         <Hero />
       </Element>
 
+      {/* Main component container */}
+      {/* Each section has reusable title */}
       <div className="container">
+        {/* About us section */}
         <Element name="about-us" className="section">
           <About title={<Title subTitle="About Us" title="who we are" />} />
         </Element>
+        {/* Image gallery for product */}
         <Element name="img-gallery" className="section">
           <Gallery
             title={<Title subTitle="Gallery" title="show me the ropes" />}
           />
         </Element>
+        {/* Contact section with form */}
         <Element name="contact-us">
           <Contact
             title={<Title subTitle="Contact Us" title="get in touch" />}
