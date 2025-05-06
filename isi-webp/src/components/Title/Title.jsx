7408dc3d2d0b3d79e13679a6ff1fe66473d3746a
Kenny Tan <kennysjtan@gmail.com>
2025-05-05 17:53:05 -0700

    comments

--- isi-webp/src/components/Title/Title.jsx
+++ isi-webp/src/components/Title/Title.jsx
@@ -1,11 +1,16 @@
+/*
+  File: Title.jsx
+  Description: Reusable title to indicate sections.
+  Author: Kendrick Tan
+*/
 import React from "react";
 import "./Title.css";
 
-const Title = ({ subTitle, title }) => {
+const Title = ({ heading, subHeading }) => {
   return (
     <div className="title">
-      <p>{subTitle}</p>
-      <h2>{title}</h2>
+      <p>{heading}</p>
+      <h2>{subHeading}</h2>
     </div>
   );
 };
