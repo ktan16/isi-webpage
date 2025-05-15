bbfe9bedc2845cca96255241f139f7b31acf7b81
Kenny Tan <kennysjtan@gmail.com>
2025-05-15 12:55:58 -0700

    cleaned imports

--- isi-webp/src/App.jsx
+++ isi-webp/src/App.jsx
@@ -4,7 +4,6 @@
   Author: Kendrick Tan
 */
 
-import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import About from "./components/About/About";
